43b3603e8764ff7a88a16dbacdc6336e45571b90
The Doggo <private>
2025-01-15 17:52:57 +0100

    debug entrypoint

--- Worker/entry.sh
+++ Worker/entry.sh
@@ -1,2 +1,7 @@
+args=()
+#test if MANAGER_URL env exists and add it to the arguments to specify a manager
+if [ -z "$MANAGER_URL"]; then #-z checks if lenght is 0
+args+=('-manager $MANAGER_URL')
+fi
 
 flamenco-worker -h
