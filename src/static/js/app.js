c1256909e567ee3f1444693209dab05082770278
vempati321 <vempati321@gmail.com>
2025-08-12 17:14:43 +0530

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">welcome to docker</p>
+                <p className="text-center">  Welcome to Docker World</p>
             )}
             {items.map(item => (
                 <ItemDisplay
